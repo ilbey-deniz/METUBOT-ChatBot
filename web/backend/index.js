fdbec2b0146da8aa039ed018bd0a4a08e040e161
Fatih <m.fatih.berberoglu@gmail.com>
2022-11-28 22:29:31 +0300

    add flask_url to backend

--- web/backend/index.js
+++ web/backend/index.js
@@ -4,6 +4,7 @@ dotenv.config();
 var path = require('path');
 var cors = require('cors')
 const app = express()
+const https = require('https');
 // parse json objects
 app.use(express.json()) 
 
@@ -14,7 +15,10 @@ const FRONTEND_FILES_LOCATION = process.env.FRONTEND_FILES_LOCATION;
 app.use(express.static(path.join(__dirname, FRONTEND_FILES_LOCATION)));
 
 app.post('/', (req, res) => {
-    console.log(req.body)
+    https.get(process.env.FLASK_URL + "/metubot/v1/example/sentence/?sentence=" + req.body, (resp) => {
+
+    })
+    // console.log(req.body)
     res.send("mesaj alındı");
 })
 
@@ -22,3 +26,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`);
 });
+
+
+
