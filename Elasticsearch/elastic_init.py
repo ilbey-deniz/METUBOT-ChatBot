e5decec5f259c7d543571958666ad36194c4dd72
BARIS AKCAM <e2448041@ceng.metu.edu.tr>
2022-12-13 15:53:48 +0000

    Refactored code and added getSimiliarQuestion to get scores

--- Elasticsearch/elastic_init.py
+++ Elasticsearch/elastic_init.py
@@ -1,45 +1,59 @@
 import json
 from elasticsearch import Elasticsearch
 
-f = open("qa_pairs.json")
-data = json.load(f)
+# Converting these functions into a class may be a good idea
 
-mapping_properties = {
-    "question": {"type": "text"},
-    "answer": {"type": "text"},
-    "join": {
-        "type": "join",
-        "relations": {"answer": "question"}
-    }
-}
+# Init elasticsearch, it will give an error when used twice since we need to delete index before using create() again
+def initQAIndex():
 
-
-es = Elasticsearch("http://localhost:9200")
-
-es.indices.create(index="question-answer")
-es.indices.put_mapping(index="question-answer", properties=mapping_properties)
-
-index_ctr=0
-for i in data ["qa-pairs"]:
-    q = {
+    es = Elasticsearch("http://localhost:9200")
+    
+    mapping_properties = {
+        "question": {"type": "text"},
+        "answer": {"type": "text"},
         "join": {
-            "name": "question",
-            "parent": str(index_ctr)
-        },
-        "body": i["question"]
+            "type": "join",
+            "relations": {"answer": "question"}
+        }
     }
-    a = {
-        "join": {
-            "name": "answer",
-        },
-        "body": i["answer"]
-    }
-    es.index(index="question-answer", document=a, id=index_ctr)
-    index_ctr = index_ctr + 1
-    es.index(index="question-answer", document=q, id=index_ctr, routing=True)
-    index_ctr = index_ctr + 1
 
+    # To delete index: curl -X DELETE "localhost:9200/question-answer"
+    es.indices.create(index="question-answer")
+    es.indices.put_mapping(index="question-answer", properties=mapping_properties)
+
+# Fill from file, overwrites starting from index 0
+def fillQAIndex(qa_file):
+
+    es = Elasticsearch("http://localhost:9200")
+
+    f = open(qa_file)
+    data = json.load(f)
+    
+    index_ctr=0
+    for i in data ["qa-pairs"]:
+        q = {
+            "join": {
+                "name": "question",
+                "parent": str(index_ctr)
+            },
+            "body": i["question"]
+        }
+        a = {
+            "join": {
+                "name": "answer",
+            },
+            "body": i["answer"]
+        }
+        es.index(index="question-answer", document=a, id=index_ctr)
+        index_ctr = index_ctr + 1
+        es.index(index="question-answer", document=q, id=index_ctr, routing=True)
+        index_ctr = index_ctr + 1
+
+# We will use this to get answer, I "guess" index 0 contains the max score result
 def getResponse(question):
+
+    es = Elasticsearch("http://localhost:9200")
+
     response = es.search(index="question-answer", query={
         "has_child": {
             "type": "question",
@@ -60,4 +74,31 @@ def getResponse(question):
     else:
         return "Hata"
 
-#print(getResponse("horde istiyom"))
+# For test purposes, returns all together with results
+def getSimiliarQuestion(question):
+
+    es = Elasticsearch("http://localhost:9200")
+
+    response = es.search(index="question-answer", query={
+        "more_like_this": {
+            "fields": ["body"],
+            "like": question,
+            "min_term_freq": 1,
+            "min_doc_freq": 1,
+            "max_query_terms": 12
+        }
+    })
+    
+    response_list = []
+    for i in response["hits"]["hits"]:
+        
+        # There may be a better solution
+        if i["_source"]["join"]["name"] == "question":
+            response_list.append((i["_id"], i["_source"]["body"], i["_score"]))
+    
+    return response_list
+
+# We need to run this file to initialize Elasticsearch before sending requests
+if __name__ == "__main__":
+    initQAIndex()
+    fillQAIndex("qa_pairs.json")
