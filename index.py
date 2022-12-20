a0a1159424cbc3f679eadc864d937425fe611e6d
ilbey-deniz <mustafa.ilbey@hotmail.com>
2022-12-21 00:44:58 +0300

    index.py updated, fasttext endpoint added

--- index.py
+++ index.py
@@ -8,6 +8,11 @@ def get_incomes():
     elastic_answerer = ElasticAnswerer()
     return elastic_answerer.answer(request.args.get('question'))
 
+@app.route('/askFast')
+def get_incomes_fast():
+    fasttext_answerer = FasttextAnswerer()
+    return fasttext_answerer.answer(request.args.get('question'))
+
 if __name__ == "__main__":
     print("NLP Backend api started. Ask your question with get request to /ask?question=your_question")
     app.run(host="0.0.0.0", port="8080", debug=False)
