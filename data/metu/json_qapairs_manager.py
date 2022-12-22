1d5613ebb8454ec6e6a905f61d886f87cdf0bd28
KAAN GOCMEN <e2414894@ceng.metu.edu.tr>
2022-12-22 21:29:57 +0300

    common json

--- data/metu/json_qapairs_manager.py
+++ data/metu/json_qapairs_manager.py
@@ -9,18 +9,23 @@ def file_to_list(file):
     with open(file) as f:
         return [line.rstrip('\n') for line in f]
 
-def add_questions_from_files(path, q_path, a_path, overwrite):
+def add_questions_from_files(path, q_path, a_path, c_path, overwrite):
     if overwrite:
         reset_and_initialize_json(path)
     questions = file_to_list(q_path)
     answers = file_to_list(a_path)
+    categories = file_to_list(c_path)
     dict = json.load(open(path))
     for i in range(len(questions)):
-        dict["qa-pairs"].append({"question":questions[i],"answer":answers[i]})
+        dict["qa-pairs"].append({"question":[questions[i]],"answer":[answers[i]],"category":categories[i]})
     write_json(dict,path)
 
-def add_questions_manually(path, question, answer):
+def add_questions_manually(path, question, answer, category):
     ...
+
+def add_alternative_question(question, answer):
+    dict = json.load(open(path))
+
 
 def write_json(dict,jsonfilepath):
 	f = open(jsonfilepath, "w")
@@ -31,4 +36,5 @@ def write_json(dict,jsonfilepath):
 path = "../../Elasticsearch/qa_pairs.json"
 q_path = "./questions.txt"
 a_path = "./answers.txt"
-add_questions_from_files(path,q_path,a_path, True)
+c_path = "./categories.txt"
+add_questions_from_files(path,q_path,a_path,c_path,True)
