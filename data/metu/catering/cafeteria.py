b07aafdb712715b2b4df407f76682250543400dd
KAAN GOCMEN <e2414894@ceng.metu.edu.tr>
2023-01-03 13:21:06 +0300

    edited yemek() as our cafeteria policy have changed, also implemented fiyat(role) it is the first example of branching questions.

--- data/metu/catering/cafeteria.py
+++ data/metu/catering/cafeteria.py
@@ -3,21 +3,51 @@ from bs4 import BeautifulSoup
 
 def initialize(url):
     page = requests.get(url)
-    #page'in class'ı <class 'requests.models.Response'> ve stringe çevirirsek html dosyası gibi oluyor.
     soup = BeautifulSoup(page.content, "html.parser")
-    #soup sayesinde page'i parse ediyoruz.
     return soup
 
 def yemek():
     soup= initialize("https://kafeterya.metu.edu.tr/")
-    a = soup.find(class_="view-content")
-    b = a.find_all(class_ = "rdf-meta element-hidden")
-    vej = a.find_all(class_ = "vejeteryan")
-    prompt = "Bugün yemekhanede "+ str(b[0].get("content")) + ", " + str(b[1].get("content")) + ", " + str(b[2].get("content")) + " ve " + str(b[3].get("content")) +  " var.\n"
-    prompt += vej[0].text.strip()
+    b = soup.find_all(class_ = "rdf-meta element-hidden")
+    vej = soup.find_all(class_ = "vejeteryan")
+    prompt = "Öğle yemeği:\n\t"+ str(b[0].get("content")) + ", " + str(b[1].get("content")) + ", " + str(b[2].get("content")) + " ve " + str(b[3].get("content")) +  "\n"
+    prompt += "\t" + vej[0].text.strip() + "\n"
+    prompt += "Akşam yemeği:\n\t"+ str(b[4].get("content")) + ", " + str(b[5].get("content")) + ", " + str(b[6].get("content")) + " ve " + str(b[7].get("content")) +  "\n"
+    prompt += "\t" + vej[1].text.strip()
+
+
     return prompt
 
 def saatler():
     soup = initialize("https://kafeterya.metu.edu.tr/yemek-saatleri")
     a = soup.find(class_="field-items")
     return a.text.strip()
+
+def fiyat(role="Öğrenci"):
+    d = {
+        "Personel: Ek Göstergesiz (Ek:0)":1,
+        "Personel: Ek Göstergeli (0 üzeri ve 2200 dahil)":2,
+        "Personel: Ek Göstergeli (2200 üzeri ve 3000 dahil)":3,
+        "Personel: Ek Göstergeli (3600 dahil ve üzeri)":4,
+        "Personel: Sözleşmeli":5,
+        "Personel: İşçi":6,
+        "Öğrenci":7,
+        "Misafir":8,
+        "Diğer":9
+    }
+
+    diff = d[role]
+
+
+    soup = initialize("http://kafeterya.metu.edu.tr/tabldot-yemek-fiyatlari")
+    a = soup.find(property= "content:encoded")
+    prompt = a.p.strong.text.strip() + "\n"
+    b = a.find(class_ ="table")
+    c= list(filter(lambda x: x != "", b.text.strip().split("\n")))
+    prompt += role + "\n"
+    prompt += c[0] + ": " + c[4*diff] + "\n"
+    prompt += c[1] + ": " + c[4*diff+1] + "\n"
+    prompt += c[2] + ": " + c[4*diff+2]
+    return prompt
+
+print(fiyat())
