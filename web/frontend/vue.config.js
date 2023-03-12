699a0eb637f184830c44117d1d5a35c8fad4fe37
lutfu <lerkaya0@gmail.com>
2023-03-12 10:56:23 +0300

    add rule for forwarding frontend dev server requests to backend dev server

--- web/frontend/vue.config.js
+++ web/frontend/vue.config.js
@@ -2,5 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    proxy: 'http://127.0.0.1:3000'
+  }
 })
