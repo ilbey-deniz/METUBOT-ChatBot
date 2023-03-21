a92da0c061b8dbfbd032decefab94c083a74cf89
lutfu <lerkaya0@gmail.com>
2023-03-21 21:23:54 +0300

    add option to send feedback text to chat messages

--- web/frontend/src/components/MetubotChatMessage.vue
+++ web/frontend/src/components/MetubotChatMessage.vue
@@ -1,82 +1,116 @@
 <template>
-    <v-menu offset-y>
-        <template v-slot:activator="{ on, attrs }">
-            <v-hover v-slot:default="{ hover }">
-                <v-sheet
-                        :color="msg.isUser ? 'primary' : 'red'"
-                        dark
-                        style="height:auto; position: relative; width: fit-content; border-radius: 19px;"
-                        class="pa-4 mb-4 message-box"
-                        v-bind="attrs"
-                        v-on="on"
-                >
-                    <v-btn
-                            style="position: absolute; right: 5px; top: 5px;"
+    <div>
+
+        <v-menu offset-y v-model="feedbackMenu">
+            <template v-slot:activator="{ on }">
+                <v-hover v-slot:default="{ hover }">
+                    <v-sheet
+                            :color="msg.isUser ? 'primary' : 'red'"
                             dark
-                            icon
-
+                            style="height:auto; position: relative; width: fit-content; border-radius: 19px;"
+                            class="pa-4 mb-4 message-box"
+                            @click="feedbackMenu = true"
                     >
-                        <v-icon v-if="hover">mdi-chevron-down</v-icon>
-                    </v-btn>
-                    <span v-html="sanitizeExceptBoldItalicCode(msg.content).trim()" v-linkified
-                          style="white-space: pre-wrap;"/>
-                    <span v-if="enableDidYouMeanThis">
-                  <template v-if="!msg.selectedDYMTQuestion">
-                     <v-alert
-                             v-for="question in msg.didYouMeanThisQuestions"
-                             color="indigo"
-                             icon="mdi-send"
-                             border="left"
-                             @click="$emit('select-dymt-question',question)"
-                             dense
-                             style="cursor: pointer; font-size:0.875rem; width:fit-content"
-                             class="mt-3"
-                     >
+                        <v-btn style="position: absolute; right: 5px; top: 5px;" dark icon>
+                            <v-icon v-if="hover">mdi-chevron-down</v-icon>
+                        </v-btn>
+                        <span v-html="sanitizeExceptBoldItalicCode(msg.content).trim()" v-linkified
+                              style="white-space: pre-wrap;"></span>
+                        <span v-if="enableDidYouMeanThis && !msg.selectedDYMTQuestion">
+                        <v-alert
+                                v-for="question in msg.didYouMeanThisQuestions"
+                                color="indigo"
+                                icon="mdi-send"
+                                border="left"
+                                @click="$emit('select-dymt-question',question)"
+                                dense
+                                style="cursor: pointer; font-size:0.875rem; width:fit-content"
+                                class="mt-3"
+                        >
                         {{ question }}
-                     </v-alert>
-                  </template>
-               </span>
-                    <sub class="ml-2"
-                         style="font-size: 0.6rem; display: block; text-align: right; color: #ddd">{{
-                            addPadding(msg.created_at)
-                        }}</sub>
+                        </v-alert>
 
-                </v-sheet>
-            </v-hover>
-        </template>
-        <v-list dense flat>
-            <v-list-item-group>
-                <v-list-item v-if="!feedback || feedback === 'like'">
+                    </span>
+                        <sub class="ml-2"
+                             style="font-size: 0.6rem; display: block; text-align: right; color: #ddd">{{
+                                addPadding(msg.created_at)
+                            }}
+                        </sub>
+                    </v-sheet>
+                </v-hover>
+            </template>
+            <v-list dense flat>
+                <v-list-item @click="onLike" v-if="!feedback || feedback === 'like'">
                     <v-list-item-icon>
                         <v-icon :color="feedback ? 'green' : ''">mdi-thumb-up</v-icon>
                     </v-list-item-icon>
                     <v-list-item-content>
-                        <v-list-item-title v-if="feedback" @click="onLike">Beğenildi</v-list-item-title>
-                        <v-list-item-title v-else @click="onLike">Beğen</v-list-item-title>
+                        <v-list-item-title v-if="feedback">Beğenildi</v-list-item-title>
+                        <v-list-item-title v-else>Beğen</v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
-                <v-list-item v-if="!feedback || feedback === 'dislike'">
+                <v-list-item @click="onDislike" v-if="!feedback || feedback === 'dislike'">
                     <v-list-item-icon>
                         <v-icon :color="feedback ? 'red' : ''">mdi-thumb-down</v-icon>
                     </v-list-item-icon>
                     <v-list-item-content>
-                        <v-list-item-title v-if="feedback" @click="onDislike">Beğenilmedi</v-list-item-title>
+                        <v-list-item-title v-if="feedback">Beğenilmedi</v-list-item-title>
                         <v-list-item-title v-else @click="onDislike">Beğenme</v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
-                <v-list-item>
+                <v-list-item @click="onFeedback">
                     <v-list-item-icon>
                         <v-icon>mdi-bug</v-icon>
                     </v-list-item-icon>
                     <v-list-item-content>
-                        <v-list-item-title @click="$emit('report-question')">Hata raporla</v-list-item-title>
+                        <v-list-item-title>Fazladan geri bildirim</v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
-            </v-list-item-group>
+            </v-list>
 
-        </v-list>
+        </v-menu>
+        <v-dialog
+                v-model="feedbackDialog"
+                max-width="600"
+        >
+            <v-card>
+                <v-card-title class="text-h5">
+                    Geri bildirim gönder
+                </v-card-title>
+                <v-card-text>
+                    <v-textarea
+                            class="mt-4"
+                            solo
+                            label="Cevap hakkında ne düşünüyorsunuz?"
+                            v-model="feedbackText"
+                    ></v-textarea>
+                </v-card-text>
 
-    </v-menu>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+
+                    <v-btn
+                            color="gray darken-1"
+                            text
+                            @click="resetFeedbackDialog"
+                    >
+                        İptal
+                    </v-btn>
+
+                    <v-btn
+                            color="green darken-1"
+                            text
+                            @click="sendFeedbackText"
+                    >
+                        Geri bildirimi gönder
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </div>
+
+
 </template>
 
 <script>
@@ -93,10 +127,10 @@ export default {
     },
     data() {
         return {
+            feedbackMenu: false,
             feedback: '',
             feedbackText: '',
-            likeDialog: false,
-            dislikeDialog: false,
+            feedbackDialog: false,
         }
     },
     methods: {
@@ -116,14 +150,29 @@ export default {
             return hour + ":" + minute;
         },
         onLike() {
-            this.feedback = 'like';
-            this.likeDialog = true;
-
+            if (this.feedback !== 'like') {
+                this.feedback = 'like';
+                this.feedbackDialog = true;
+            }
 
         },
         onDislike() {
-            this.feedback = 'dislike';
-            this.dislikeDialog = true;
+            if (this.feedback !== 'dislike') {
+                this.feedback = 'dislike';
+                this.feedbackDialog = true;
+            }
+        },
+        onFeedback() {
+            this.$emit('report-question');
+            this.feedbackDialog = true;
+        },
+        resetFeedbackDialog() {
+            this.feedbackText = '';
+            this.feedbackDialog = false;
+        },
+        sendFeedbackText() {
+            this.resetFeedbackDialog();
+
         }
 
     },
