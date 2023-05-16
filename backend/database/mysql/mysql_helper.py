4a31d6fe2bdfe471b478409b430d5661144da36c
Lütfullah Erkaya <lerkaya0@gmail.com>
2023-05-17 00:07:48 +0300

    small fixes

--- backend/database/mysql/mysql_helper.py
+++ backend/database/mysql/mysql_helper.py
@@ -20,8 +20,8 @@ class AskedQuestion(Base):
     __tablename__ = "asked_questions"
 
     Qid = Column(Integer, primary_key=True)
-    asked_question = Column(String(511))
-    answer = Column(String(512))
+    asked_question = Column(Text)
+    answer = Column(Text)
     similarity = Column(FLOAT)
     category = Column(String(60))
     created_at = Column(DateTime, default=datetime.now)
--- misc-helper-scripts/fix_asked_question_column_size.py
+++ misc-helper-scripts/fix_asked_question_column_size.py
@@ -6,12 +6,11 @@ from backend.database.mysql.connector import *
 session = create_session()
   
 table_name = 'asked_questions'
-  # change varchar to text
-query = f"ALTER TABLE {table_name} MODIFY COLUMN question TEXT"
-query = f"ALTER TABLE {table_name} MODIFY COLUMN answer TEXT"
+
+query = f"ALTER TABLE {table_name} MODIFY asked_question TEXT"
+query = f"ALTER TABLE {table_name} MODIFY answer TEXT"
+
 session.execute(query)
-
-
 
 session.commit()
 session.close()
