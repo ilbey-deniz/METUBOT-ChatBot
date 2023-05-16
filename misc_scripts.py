5d51546a50bd1ca910e0e70a22638eed73155480
Lütfullah Erkaya <lerkaya0@gmail.com>
2023-05-17 01:21:27 +0300

    script refactor

--- misc_scripts.py
+++ misc_scripts.py
@@ -3,14 +3,17 @@ from backend.database.mysql.connector import *
 
   
 def fix_asked_question_column_size():
-    session = create_session()
+    engine = get_engine()
+    connection = engine.connect()
+  
     
     table_name = 'asked_questions'
 
     query = f"ALTER TABLE {table_name} MODIFY asked_question TEXT"
     query = f"ALTER TABLE {table_name} MODIFY answer TEXT"
+    
 
-    session.execute(query)
+    connection.execute(query)
 
-    session.commit()
-    session.close()
+
+
